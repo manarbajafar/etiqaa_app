e7d75193584ad1e37ce6618f27c2cc54fa609561
maram46 <87251947+maram46@users.noreply.github.com>
2023-01-16 18:16:31 +0300

    update alertDetails

--- etiqaa/lib/screens/alertDetails.dart
+++ etiqaa/lib/screens/alertDetails.dart
@@ -1,3 +1,5 @@
+import 'package:etiqaa/database/crud.dart';
+import 'package:etiqaa/database/linkApi.dart';
 import 'package:etiqaa/screens/alertHistory.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,15 +8,33 @@ import '../models/child.dart';
 import '../models/message.dart';
 import 'homepage.dart';
 
+late int idM;
+late bool save;
+
 class AlertDetails extends StatelessWidget {
-  final int idList;
+  final int id;
+  final String name;
+  final String date;
+  final String sender;
+  final String content;
+  final Gender gender;
+  final bool isSaved;
+
   const AlertDetails({
     Key? key,
-    required this.idList,
+    required this.name,
+    required this.date,
+    required this.sender,
+    required this.content,
+    required this.gender,
+    required this.id,
+    required this.isSaved,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    save = isSaved;
+    idM = id;
     bool isVisible = true;
     return Scaffold(
       body: Column(
@@ -34,10 +54,10 @@ class AlertDetails extends StatelessWidget {
                       alignment: Alignment.topRight,
                       child: IconButton(
                         icon: Icon(Icons.close),
-                        iconSize: 10.w,
+                        iconSize: 25.w,
                         color: Colors.black,
                         onPressed: () {
-                          Get.offAll(HomePage());
+                          Get.back();
                         },
                       ),
                     ),
@@ -49,14 +69,14 @@ class AlertDetails extends StatelessWidget {
                     padding: EdgeInsets.symmetric(horizontal: 10.w),
                     child: Row(
                       children: [
-                        messageList[idList].childgender == Gender.Boy
+                        gender == Gender.Boy
                             ? Image.asset('images/boyIcon_c.png')
                             : Image.asset('images/girlIcon_c.png'),
                         Padding(
                           padding: EdgeInsets.symmetric(
                               horizontal: 10.w, vertical: 5.h),
                           child: Text(
-                            '${messageList[idList].childName}',
+                            '$name',
                             style: Theme.of(context).textTheme.headline1,
                           ),
                         )
@@ -70,132 +90,140 @@ class AlertDetails extends StatelessWidget {
           Padding(
             padding: EdgeInsets.only(top: 15.h),
             child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      'الوقت',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      '${messageList[idList].date}',
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.h,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      'المرسل',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      '${messageList[idList].senderName}',
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.h,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      'العبارة',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.w),
-                    child: Text(
-                      '${messageList[idList].message}',
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.h),
-                  child: Center(
-                    child: Text(
-                      'هل تريد حفظ الرسالة؟',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.h),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        height: 40.h,
-                        width: 100.w,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            // ignore: deprecated_member_use
-                            primary: const Color(0xF0F9AF4B),
-                          ),
-                          onPressed: () {
-                            Get.to(AlertHistory(), arguments: [1]);
-                          },
-                          child: Text(
-                            'نعم',
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                        ),
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        'الوقت',
+                        style: Theme.of(context).textTheme.headline1,
                       ),
-                      SizedBox(
-                        height: 40.h,
-                        width: 100.w,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            // ignore: deprecated_member_use
-                            primary: const Color(0xF0F9AF4B),
-                          ),
-                          onPressed: () {
-                            SetState() {
-                              isVisible = !isVisible;
-                            }
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        '$date',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        'المرسل',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        '$sender',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        'العبارة',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: Text(
+                        '$content',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  ),
+                  save == false
+                      ? Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(top: 10.h),
+                              child: Center(
+                                child: Text(
+                                  'هل تريد حفظ الرسالة؟',
+                                  style: Theme.of(context).textTheme.headline1,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20.h),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    height: 40.h,
+                                    width: 100.w,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        // ignore: deprecated_member_use
+                                        primary: const Color(0xF0F9AF4B),
+                                      ),
+                                      onPressed: () {
+                                        saveMessage();
+                                      },
+                                      child: Text(
+                                        'نعم',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline4,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 40.h,
+                                    width: 100.w,
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        // ignore: deprecated_member_use
+                                        primary: const Color(0xF0F9AF4B),
+                                      ),
+                                      onPressed: () {
+                                        deleteMessage();
 
-                            Get.off(HomePage());
-                          },
-                          child: Text(
-                            'لا',
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                                        Get.off(HomePage());
+                                      },
+                                      child: Text(
+                                        'لا',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline4,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      : Container(),
+                ]),
           ),
         ],
       ),
@@ -203,26 +231,33 @@ class AlertDetails extends StatelessWidget {
   }
 }
 
-List<Message> messageList = [
-  Message(
-      id: 1,
-      childName: 'سارة',
-      childgender: Gender.Girl,
-      message: 'غبية وقذرة',
-      date: '2022-10-07 10:30',
-      senderName: 'ريم محمد'),
-  Message(
-      id: 2,
-      childName: 'سارة',
-      childgender: Gender.Girl,
-      message: 'لاتقول لامك',
-      date: '2022-10-06 10:00',
-      senderName: 'ريم محمد'),
-  Message(
-      id: 3,
-      childName: 'عمر',
-      childgender: Gender.Boy,
-      message: 'اطعنك',
-      date: '2022-10-06 10:00',
-      senderName: 'ريم محمد'),
-];
+Crud _crud = Crud();
+deleteMessage() async {
+  var response = await _crud.postRequest2(linkdeleteMessages, {
+    'msg_id': idM.toString(),
+  });
+
+  // response = jsonDecode(response.toString());
+  print(response.toString());
+  if (response != null && response["statues"] == "success") {
+    save = true;
+  } else {
+    print("delete fail");
+  }
+}
+
+saveMessage() async {
+  var response = await _crud.postRequest2(linkSaveMessage, {
+    'msg_id': idM.toString(),
+  });
+
+  // response = jsonDecode(response.toString());
+  print(response.toString());
+  if (response != null && response["statues"] == "success") {
+    save = true;
+  } else {
+    print("save fail");
+  }
+}
+
+List<Message> messageList = [];
