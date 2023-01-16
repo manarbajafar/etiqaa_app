d2f8b37b05be1d2ca3ccd1a2edce50e679f95bea
maram46 <87251947+maram46@users.noreply.github.com>
2023-01-16 18:17:22 +0300

    update alertHistory

--- etiqaa/lib/screens/alertHistory.dart
+++ etiqaa/lib/screens/alertHistory.dart
@@ -1,39 +1,110 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:etiqaa/widgets/messagesCards.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../database/crud.dart';
+import '../database/linkApi.dart';
+import '../main.dart';
 import '../models/child.dart';
 import '../models/message.dart';
 import '../widgets/curvedAppbar.dart';
 import 'accountSettings.dart';
 import 'advice.dart';
+import 'alertDetails.dart';
 import 'homepage.dart';
 
-class AlertHistory extends StatelessWidget {
-  int data = 1;
-  List<Message> messageList = [
-    Message(
-        id: 1,
-        childName: 'سارة',
-        childgender: Gender.Girl,
-        message: 'غبية وقذرة',
-        date: '2022-10-07 10:30',
-        senderName: 'ريم محمد'),
-    Message(
-        id: 2,
-        childName: 'سارة',
-        childgender: Gender.Girl,
-        message: 'لاتقول لامك',
-        date: '2022-10-06 10:00',
-        senderName: 'ريم محمد'),
-    Message(
-        id: 3,
-        childName: 'عمر',
-        childgender: Gender.Boy,
-        message: 'اطعنك',
-        date: '2022-10-06 10:00',
-        senderName: 'ريم محمد'),
-  ];
+Gender getGender(String gender) {
+  switch (gender) {
+    case 'ذكر':
+      return Gender.Boy;
+    case 'أنثى':
+      return Gender.Girl;
+    default:
+      return Gender.Boy;
+  }
+}
+
+Crud _crud = Crud();
+
+List msgidi = [];
+int msgSaved = 0;
+List msgchild = [];
+bool msg = false;
+messages() async {
+  List response = await _crud.getRequest(linkAllMessages);
+  for (int i = 0; i < response.length; i++) {
+    if (response[i]['parent_id'].toString() ==
+        sharedPref.getString('parent_id')) {
+      msgidi.add(response[i]['msg_id']);
+      msgchild.add(response[i]['child_name']);
+      msg = true;
+    } else {}
+  }
+  if (msg == true) {
+    allMessages();
+  }
+}
+
+List<Message> messageList = [];
+
+int msgNumber = 0;
+allMessages() async {
+  messageList = [];
+
+  print(msgidi.toString());
+  print(msgchild.toString());
+
+  for (int i = 0; i < msgidi.length; i++) {
+    var response = await _crud.postRequest(linkAlertHistory, {
+      'msg_id': (msgidi[i]),
+    });
+    print(response.toString());
+    if (response != null && response[0]["statues"] == "success") {
+      var responseChild = await _crud.postRequest2(linkChild, {
+        'child_name': msgchild[i].toString(),
+        'parent_id': sharedPref.getString('parent_id'),
+      });
+      print(responseChild.toString());
+      if (responseChild != null && responseChild[0]['statues'] == "success") {
+        // AsyncSnapshot snap = responseChild;
+        print(response.toString());
+        // response = jsonEncode(response.toString());
+        print(responseChild[0]['gender']);
+        messageList.add(
+          Message(
+              id: int.parse(msgidi[i]),
+              childName: msgchild[i],
+              childgender: getGender(responseChild[0]['gender']),
+              message: response[0]['content'],
+              date: response[0]['date_time'].toString(),
+              senderName: response[0]['sender'],
+              isSaved: true),
+        );
+        msgNumber++;
+      } else {
+        print('Child fail');
+      }
+    } else {
+      print('Msg fail');
+    }
+  }
+}
+
+class AlertHistory extends StatefulWidget {
+  @override
+  State<AlertHistory> createState() => _AlertHistoryState();
+}
+
+class _AlertHistoryState extends State<AlertHistory> {
+  @override
+  void initState() {
+    msgidi = [];
+    msgchild = [];
+    messages();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,80 +183,159 @@ class AlertHistory extends StatelessWidget {
           'سجل التنبيهات',
           style: Theme.of(context).textTheme.headline1,
         ),
-        Expanded(
-          flex: 1,
-          child: ListView.builder(
-            reverse: false,
-            shrinkWrap: true,
-            itemCount: 1,
-            itemBuilder: ((context, index) {
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      height: 210,
-                      width: 350,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(40),
-                        child: Card(
-                            color: Color.fromRGBO(255, 255, 255, 1),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Column(
+         Expanded(
+                          flex: 1,
+                          child: ListView.builder(
+                            reverse: false,
+                            shrinkWrap: true,
+                            itemCount: msgNumber,
+                            itemBuilder: ((context, index) {
+                              return Column(
                                 children: [
-                                  Container(
-                                    height: 100,
-                                    color: Color.fromRGBO(237, 236, 242, 1),
-                                    child: Row(children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: messageList[data].childgender ==
-                                                Gender.Boy
-                                            ? Image.asset(
-                                                'images/boyIcon_c.png')
-                                            : Image.asset(
-                                                'images/girlIcon_c.png'),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 20.w, vertical: 10.h),
+                                    child: SizedBox(
+                                      height: 170.h,
+                                      width: 280.w,
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(40.r),
+                                        child: Card(
+                                            color: Color.fromRGBO(
+                                                255, 255, 255, 1),
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(20.r),
+                                              child: Column(
+                                                children: [
+                                                  Container(
+                                                    height: 45.h,
+                                                    color: Color.fromRGBO(
+                                                        237, 236, 242, 1),
+                                                    child: Row(children: [
+                                                      Padding(
+                                                        padding: EdgeInsets
+                                                            .symmetric(
+                                                          horizontal: 10.w,
+                                                        ),
+                                                        child: messageList[
+                                                                        index]
+                                                                    .childgender ==
+                                                                Gender.Boy
+                                                            ? Image.asset(
+                                                                'images/boyIcon_c.png')
+                                                            : Image.asset(
+                                                                'images/girlIcon_c.png'),
+                                                      ),
+                                                      Padding(
+                                                        padding: EdgeInsets
+                                                            .symmetric(
+                                                                vertical: 2.h),
+                                                        child: Text(
+                                                          '${messageList[index].childName}',
+                                                          style:
+                                                              Theme.of(context)
+                                                                  .textTheme
+                                                                  .headline1,
+                                                        ),
+                                                      )
+                                                    ]),
+                                                  ),
+                                                  Align(
+                                                    alignment:
+                                                        Alignment.topRight,
+                                                    child: Padding(
+                                                      padding:
+                                                          EdgeInsets.symmetric(
+                                                              horizontal: 15.w),
+                                                      child: Text(
+                                                        'العبارة',
+                                                        style: Theme.of(context)
+                                                            .textTheme
+                                                            .headline1,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Align(
+                                                    alignment:
+                                                        Alignment.topRight,
+                                                    child: Padding(
+                                                      padding:
+                                                          EdgeInsets.symmetric(
+                                                              horizontal: 15.w),
+                                                      child: Text(
+                                                        '${messageList[index].message}',
+                                                        style: Theme.of(context)
+                                                            .textTheme
+                                                            .headline5,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.symmetric(
+                                                            horizontal: 10.w),
+                                                    child: Align(
+                                                      alignment:
+                                                          Alignment.topLeft,
+                                                      child: TextButton(
+                                                        onPressed: () {
+                                                          Get.off(
+                                                            AlertDetails(
+                                                              name: messageList[
+                                                                      index]
+                                                                  .childName,
+                                                              date: messageList[
+                                                                      index]
+                                                                  .date,
+                                                              sender: messageList[
+                                                                      index]
+                                                                  .senderName,
+                                                              content:
+                                                                  messageList[
+                                                                          index]
+                                                                      .message,
+                                                              gender: (messageList[
+                                                                      index]
+                                                                  .childgender),
+                                                              isSaved:
+                                                                  messageList[
+                                                                          index]
+                                                                      .isSaved,
+                                                              id: messageList[
+                                                                      index]
+                                                                  .id,
+                                                            ),
+                                                          );
+                                                        },
+                                                        child: Text(
+                                                          'المزيد',
+                                                          style: TextStyle(
+                                                            color: Colors.black,
+                                                            fontSize: 15.sp,
+                                                            fontFamily:
+                                                                'FFHekaya',
+                                                            decoration:
+                                                                TextDecoration
+                                                                    .underline,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            )),
                                       ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          '${messageList[data].childName}',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline2,
-                                        ),
-                                      )
-                                    ]),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 200.0),
-                                    child: Text(
-                                      'العبارة',
-                                      style:
-                                          Theme.of(context).textTheme.headline2,
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 200.0),
-                                    child: Text(
-                                      '${messageList[data].message}',
-                                      style:
-                                          Theme.of(context).textTheme.headline6,
-                                    ),
-                                  )
                                 ],
-                              ),
-                            )),
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            }),
-          ),
-        ),
-      ]),
+                              );
+                            }),
+                          ),
+                        ),
+                      ]),
     );
   }
 }
